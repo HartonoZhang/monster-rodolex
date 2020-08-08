77af7581f5017ebebc90bac2c6b7fdb8ed92faf7
HartonoZhang <hartonobestzx@gmail.com>
2020-08-08 11:41:36 +0700

    Updates

---
+++ precache-manifest.e6c98710b42355250d1cbf516dadf150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4fb227390656acaab66562ba046efc",
+    "url": "/monster-rodolex/index.html"
+  },
+  {
+    "revision": "e390c5f927b601bff883",
+    "url": "/monster-rodolex/static/css/main.b38d4221.chunk.css"
+  },
+  {
+    "revision": "c9aeccf2d6795d1396db",
+    "url": "/monster-rodolex/static/js/2.c0ace3de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rodolex/static/js/2.c0ace3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e390c5f927b601bff883",
+    "url": "/monster-rodolex/static/js/main.9ee21fa2.chunk.js"
+  },
+  {
+    "revision": "cec64ebb786dc807d8a3",
+    "url": "/monster-rodolex/static/js/runtime-main.939a7761.js"
+  }
+]);
